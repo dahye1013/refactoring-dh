f3e7481c2649de0ba48f374651bc0bc0ec7f4178
dahye1013 <alrean@naver.com>
2021-12-19 23:23:07 +0900

    [ch07](refactor) 7-3 기본형을 객체로 바꾸기 - 가다듬기, 검증로직 추가

--- 07-chapter/sample/7-3-기본형을객체로바꾸기.js
+++ 07-chapter/sample/7-3-기본형을객체로바꾸기.js
@@ -4,10 +4,30 @@ class Priority {
   }
 
   constructor(value) {
-    this._value = value;
+    //인스턴스 검사
+    if (value instanceof Priority) return value;
+    //유효 타입 검사
+    if (Priority.legalValues().includes(value)) this._value = value;
+    else throw new Error(`<${value}> is invalid for Priority`);
   }
   toString() {
     return this._value;
+  }
+  get _index() {
+    return Priority.legalValues().findIndex((s) => s === this._value);
+  }
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+
+  equals(other) {
+    return this._index === other._index;
+  }
+  higherThan(other) {
+    return this._index > other._index;
+  }
+  lowerThan(other) {
+    return this._index < other._index;
   }
 }
 
@@ -25,8 +45,8 @@ export class Order {
     return this._priority.toString(); //우선순위를 표현하는 문자열 - 함수 선언바꾸기
   }
 
-  set priorityString(value) {
-    this._priority = new Priority(value);
+  set priorityString(aString) {
+    this._priority = new Priority(aString);
   }
 }
 
