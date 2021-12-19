1688f361ee0af6596e76de389a3c49ae0cff0b61
dahye1013 <alrean@naver.com>
2021-12-19 19:56:01 +0900

    [ch07](refactor) 간단한 레코드 캡슐화하기

--- 07-chapter/sample/7-1-레코드캡슐화하기.js
+++ 07-chapter/sample/7-1-레코드캡슐화하기.js
@@ -1,1 +1,33 @@
-export const organization = { name: "Acme Gooseberries", country: "GB" };
+class Organization {
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+  set name(aString) {
+    this._name = aString;
+  }
+  get name() {
+    return this._name;
+  }
+  get country() {
+    return this._country;
+  }
+  set country(value) {
+    this._country = value;
+  }
+}
+
+export const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
+
+/**
+ * 상수 캡슐화하기
+ * @returns
+ */
+export function getRawDataOfOrganization() {
+  return organization;
+}
+
+
--- 07-chapter/test/7-1-레코드캡슐화하기.test.js
+++ 07-chapter/test/7-1-레코드캡슐화하기.test.js
@@ -1,10 +1,17 @@
-import { organization } from "../sample/7-1-레코드캡슐화하기";
+import {
+  organization,
+  getRawDataOfOrganization,
+} from "../sample/7-1-레코드캡슐화하기";
 
 const organizationName = "Acme Gooseberries";
 
 describe("[Chpater07.1 레코드 캡슐화]", () => {
-  test("", () => {
+  test("기본 레코드", () => {
     let result = `<h1>${organization.name}</h1>`; //읽기
-    organization.name = organizationName; //쓰기
+    expect(organization.name).toEqual(organizationName); //쓰기
+  });
+  test("상수 캡슐화하기", () => {
+    let result = `<h1>${organization.name}</h1>`; //읽기
+    expect(getRawDataOfOrganization().name).toEqual(organizationName); //쓰기
   });
 });
