b97a660759ff8ad7c9fc2436547ec4d1741a9b43
dahye1013 <alrean@naver.com>
2021-12-19 20:21:12 +0900

    [ch07](refactor) 7-1 중첩레코드 캡슐화하기 기본 코드

--- 07-chapter/sample/7-1-레코드캡슐화하기.js
+++ 07-chapter/sample/7-1-레코드캡슐화하기.js
@@ -1,3 +1,5 @@
+import { func } from "prop-types";
+
 class Organization {
   constructor(data) {
     this._name = data.name;
@@ -30,4 +32,28 @@ export function getRawDataOfOrganization() {
   return organization;
 }
 
-
+export let customerData = {
+  1920: {
+    name: "martin",
+    id: "1920",
+    usages: {
+      2016: {
+        1: 50,
+        2: 55, // 나머지 달 생략
+      },
+      2015: {
+        1: 70,
+        2: 63, // 나머지 달 생략
+      },
+    },
+  },
+};
+const amount = 10;
+//쓰기 - customerData[customerID].usages[year][month] = amount;
+customerData[1920].usages[2016][3] = amount;
+//읽기
+export function compareUsage(customerID, laterYear, month) {
+  const later = customerData[customerID].usages[laterYear][month];
+  const earlier = customerData[customerID].usages[laterYear - 1][month];
+  return { laterAmount: later, change: later - earlier };
+}
--- 07-chapter/test/7-1-레코드캡슐화하기.test.js
+++ 07-chapter/test/7-1-레코드캡슐화하기.test.js
@@ -1,17 +1,33 @@
 import {
   organization,
   getRawDataOfOrganization,
+  //
+  customerData,
+  compareUsage,
 } from "../sample/7-1-레코드캡슐화하기";
 
 const organizationName = "Acme Gooseberries";
 
 describe("[Chpater07.1 레코드 캡슐화]", () => {
-  test("기본 레코드", () => {
-    let result = `<h1>${organization.name}</h1>`; //읽기
-    expect(organization.name).toEqual(organizationName); //쓰기
+  describe("07-1-1 간단한 레코드 캡슐화", () => {
+    test("기본 레코드 - Before Refactoring", () => {
+      expect(organization.name).toEqual(organizationName);
+    });
+    test("상수 캡슐화하기 - After Refactoring", () => {
+      expect(getRawDataOfOrganization().name).toEqual(organizationName); //쓰기
+    });
   });
-  test("상수 캡슐화하기", () => {
-    let result = `<h1>${organization.name}</h1>`; //읽기
-    expect(getRawDataOfOrganization().name).toEqual(organizationName); //쓰기
+
+  describe("07-1-2 중첩된 레코드 캡슐화", () => {
+    test("기본 레코드 - Before Refactoring", () => {
+      const amount = 10;
+      const customerID = 1920;
+      const year = 2016;
+      const month = 3;
+      customerData[customerID].usages[year][month] = amount; //쓰기
+      expect(compareUsage(customerID, year, month).laterAmount).toEqual(amount);
+    });
   });
+
+  //
 });
