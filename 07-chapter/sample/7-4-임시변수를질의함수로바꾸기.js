87cfd265c90e2b79ec35b136ad8dd72818458f7c
dahye1013 <alrean@naver.com>
2021-12-20 22:10:12 +0900

    [ch07](refactor)7-4 임시 변수를 질의 함수로 바꾸기 - 변수 인라인하기, 함수 추출하기

--- 07-chapter/sample/7-4-임시변수를질의함수로바꾸기.js
+++ 07-chapter/sample/7-4-임시변수를질의함수로바꾸기.js
@@ -4,9 +4,22 @@ class Order {
     this._item = item;
   }
   get price() {
-    var basePrice = this._quantity * this._item.price;
+    //1.읽기전용 var -> const
+    // const basePrice = this.basePrice;
+    // const discountFactor = 0.98;
+    // if (this.basePrice > 1000) discountFactor -= 0.03;
+
+    return this.basePrice * this.discountFactor;
+  }
+
+  //2.Getter로 추출
+  get basePrice() {
+    return this._quantity * this._item.price;
+  }
+
+  get discountFactor() {
     var discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return discountFactor;
   }
 }
