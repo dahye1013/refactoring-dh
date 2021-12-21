1196fea75a974310f8d4732b80d1f44fa4f1754b
dahye1013 <alrean@naver.com>
2021-12-22 00:37:16 +0900

    [ch07](refactor)7-7 위임 숨기기 - 기본코드

---
+++ 07-chapter/sample/7-7-위임숨기기.js
@@ -0,0 +1,33 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  get department() {
+    return this._department;
+  }
+  set department(arg) {
+    this._department = arg;
+  }
+}
+
+class Departement {
+  constructor() {}
+  get chargeCode() {
+    return this._chargeCode;
+  }
+  set chargeCode(arg) {
+    this._chargeCode = arg;
+  }
+  get manager() {
+    return this._manager;
+  }
+  set manager(arg) {
+    return (this._manager = arg);
+  }
+}
+
+const aPerson = new Person();
+const manager = aPerson.department.manager;
