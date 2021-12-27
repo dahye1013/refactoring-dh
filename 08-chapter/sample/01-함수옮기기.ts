3a4ceca48df84943d63e4aa2a37ba2a130930850
dahye1013 <alrean@naver.com>
2021-12-27 22:15:07 +0900

    [ch08](sample) 8-1 함수 옮기기 - 타깃 컨텍스트로 복사

--- 08-chapter/sample/01-함수옮기기.ts
+++ 08-chapter/sample/01-함수옮기기.ts
@@ -1,9 +1,13 @@
 {
-  const distance = (p1: number, p2: number): number => Math.abs(p1 - p2);
-  const calculateTime = () => 10000;
-
   const trackSummary = (points: number[]): object => {
-    const calculateDistance = () => {
+    //(내부로 함수 옮김) - 사용하는 컨텍스트 안으로
+    const distance = (p1: number, p2: number): number => Math.abs(p1 - p2);
+    const calculateTime = (): number => 10000;
+
+    //(중첩 함수를 최상위로 올리기)
+    //- const로 선언해서 function이 아닌 호이스팅으로 인한 메모리 참조가 불가하여, 미리 위에 선언해둠.
+    //-> 최상위로 복사하면서 새로운 임시 이름 지어줌
+    const top_calculateDistance = (points: number[]) => {
       let result = 0;
       for (let i = 1; i < points.length; i++) {
         result += distance(points[i - 1], points[i]);
@@ -11,9 +15,9 @@
       return result;
     };
 
-    const totalTime: number = calculateTime();
-    const totalDistance: number = calculateDistance();
-    const pace = totalTime / 60 / totalDistance;
+    const totalTime: number = calculateTime(); //숫자
+    const totalDistance = top_calculateDistance; //함수 할당
+    const pace = totalTime / 60 / totalDistance(points);
 
     return {
       time: totalTime,
