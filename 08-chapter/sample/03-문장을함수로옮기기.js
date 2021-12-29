b72e3c52c7d41064c4fc57f6fd9362e240c0f4e2
dahye1013 <alrean@naver.com>
2021-12-29 19:00:46 +0900

    [ch08](refactor) 8-3 문장을 함수로 옮기기 - 함수 이름바꾸기

--- 08-chapter/sample/03-문장을함수로옮기기.js
+++ 08-chapter/sample/03-문장을함수로옮기기.js
@@ -2,7 +2,6 @@ function renderPerson(person) {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(zzem(person.photo)); //--추출함수
   result.push(emitPhotoData(person.photo));
   return result.join("\n");
 }
@@ -11,20 +10,12 @@ function photoDiv(aPhoto) {
   return ["<div>", emitPhotoData(aPhoto), "</div>"].join("\n");
 }
 
-//함수추출 + emitPhotoData 함수 인라인
-function zzem(p) {
+//함수추출 + emitPhotoData 함수 인라인 + 함수이름바꾸기
+function emitPhotoData(p) {
   return [
     `<p>title: ${p.title}</p>`,
     `<p>location: ${p.location}</p>`,
     `<p>date: ${p.date.toDateString()}</p>`,
-  ].join("\n");
-}
-
-function emitPhotoData(aPhoto) {
-  return [
-    `<p>title: ${aPhoto.title}</p>`,
-    `<p>location: ${aPhoto.location}</p>`,
-    `<p>date: ${aPhoto.date.toDateString()}</p>`,
   ].join("\n");
 }
 
