b85e5eb7762accd326ab7a2828914c02c6b6fc78
dahye1013 <alrean@naver.com>
2022-01-09 12:48:24 +0900

    [ch10] 10-04 조건부로직 다형성 - 함수를 클래스에 묶기

--- 10-chapter/sample/04-조건부로직-다형성으로바꾸기.js
+++ 10-chapter/sample/04-조건부로직-다형성으로바꾸기.js
@@ -7,6 +7,41 @@ const birds = [
   { name: "NorwegianBlueParrot2", type: "NorwegianBlueParrot", isNailed: true, voltage: 50 },
 ];
 
+/************************************************************************************************ */
+//Bird Class 
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+
+  get plumage() {
+    switch (this.type) {
+      case "EuropeanSwallow":
+        return "average";
+      case "AfricanSwallow":
+        return this.numberOfCoconuts > 2 ? "tired" : "average";
+      case "NorwegianBlueParrot":
+        return this.voltage > 100 ? "scorched" : "beautiful";
+      default:
+        return "unknown";
+    }
+  }
+
+  get airSpeedVelocity() {
+    switch (this.type) {
+      case "EuropeanSwallow":
+        return 35;
+      case "AfricanSwallow":
+        return 40 - 2 * this.numberOfCoconuts;
+      case "NorwegianBlueParrot":
+        return this.isNailed ? 0 : 10 + this.voltage / 10;
+      default:
+        return null;
+    }
+  }
+}
+
+/************************************************************************************************ */
 /**
  * Speed 계산 로직
  * @param {*} birds
@@ -14,16 +49,7 @@ const birds = [
  */
 const speed = (birds) => new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
 const airSpeedVelocity = (bird) => {
-  switch (bird.type) {
-    case "EuropeanSwallow":
-      return 35;
-    case "AfricanSwallow":
-      return 40 - 2 * bird.numberOfCoconuts;
-    case "NorwegianBlueParrot":
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
-  }
+  return new Bird(bird).airSpeedVelocity;
 };
 
 /**
@@ -32,16 +58,7 @@ const airSpeedVelocity = (bird) => {
  * @returns
  */
 const plumage = (bird) => {
-  switch (bird.type) {
-    case "EuropeanSwallow":
-      return "average";
-    case "AfricanSwallow":
-      return bird.numberOfCoconuts > 2 ? "tired" : "average";
-    case "NorwegianBlueParrot":
-      return bird.voltage > 100 ? "scorched" : "beautiful";
-    default:
-      return "unknown";
-  }
+  return new Bird(bird).plumage;
 };
 const plumages = (birds) => {
   return new Map(birds.map((b) => [b.name, plumage(b)]));
@@ -49,23 +66,23 @@ const plumages = (birds) => {
 
 console.log(speed(birds));
 /**
- * Spped 결과
- * 
- * Map { 'EuropeanSwallow' => 35,
-  'AfricanSwallow1' => 36,
-  'AfricanSwallow2' => 32,
-  'NorwegianBlueParrot1' => 310,
-  'NorwegianBlueParrot2' => 0 }
-  ​​​​​at ​​​​​​​​speed(birds)​​​
- * 
- */
+   * Spped 결과
+   * 
+   * Map { 'EuropeanSwallow' => 35,
+    'AfricanSwallow1' => 36,
+    'AfricanSwallow2' => 32,
+    'NorwegianBlueParrot1' => 310,
+    'NorwegianBlueParrot2' => 0 }
+    ​​​​​at ​​​​​​​​speed(birds)​​​
+   * 
+   */
 console.log(plumages(birds));
 /**
- * plumage 결과
- * Map { 'EuropeanSwallow' => 'average',
-  'AfricanSwallow1' => 'average',
-  'AfricanSwallow2' => 'tired',
-  'NorwegianBlueParrot1' => 'scorched',
-  'NorwegianBlueParrot2' => 'beautiful' }
-  ​​​​​at ​​​​​​​​plumages(birds)
- */
+   * plumage 결과
+   * Map { 'EuropeanSwallow' => 'average',
+    'AfricanSwallow1' => 'average',
+    'AfricanSwallow2' => 'tired',
+    'NorwegianBlueParrot1' => 'scorched',
+    'NorwegianBlueParrot2' => 'beautiful' }
+    ​​​​​at ​​​​​​​​plumages(birds)
+   */
