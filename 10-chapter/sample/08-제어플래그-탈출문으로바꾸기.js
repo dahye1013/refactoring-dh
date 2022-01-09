f3f2b34ab53848fedb6aef6bc88923680a9f6aee
dahye1013 <alrean@naver.com>
2022-01-09 22:43:08 +0900

    [ch10] 제어플래그 탈출문으로 - some 함수 사용

--- 10-chapter/sample/08-제어플래그-탈출문으로바꾸기.js
+++ 10-chapter/sample/08-제어플래그-탈출문으로바꾸기.js
@@ -1,9 +1,6 @@
 const sendAlert = () => console.warn("악당 발견😎");
 
 const checkForMiscreants = (people) => {
-  for (const p of people) {
-    if (p === "조커") return sendAlert();
-    if (p === "사루만") return sendAlert();
-  }
+  if (people.some((p) => ["조커", "사루만"].includes(p))) sendAlert();
 };
 checkForMiscreants(["슈퍼맨", "배트맨", "아이언맨", "사루만", "블랙위도우", "조커", "스파이더맨"]);
