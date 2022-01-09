bbadbf40846032c23ba474dddf3273c97bf77d17
dahye1013 <alrean@naver.com>
2022-01-09 22:30:35 +0900

    [ch10] 제어플래그 탈출문으로 - 기본코드

---
+++ 10-chapter/sample/08-제어플래그-탈출문으로바꾸기.js
@@ -0,0 +1,18 @@
+const sendAlert = () => console.warn("악당 발견😎");
+
+const checkForMiscreants = (people) => {
+  let found = false;
+  for (const p of people) {
+    if (!found) {
+      if (p === "조커") {
+        sendAlert();
+        found = true;
+      }
+      if (p === "사루만") {
+        sendAlert();
+        found = true;
+      }
+    }
+  }
+};
+checkForMiscreants(["슈퍼맨", "배트맨", "아이언맨", "사루만", "블랙위도우", "조커", "스파이더맨"]);
