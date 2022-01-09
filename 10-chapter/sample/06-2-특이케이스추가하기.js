d98a6b9ea10a049c8c01a5c6834e0641657d6e1d
dahye1013 <alrean@naver.com>
2022-01-09 19:59:37 +0900

    [ch10] 특이케이스2 - 기본코드

---
+++ 10-chapter/sample/06-2-특이케이스추가하기.js
@@ -0,0 +1,49 @@
+class Site {
+  constructor(customer) {
+    this._customer = customer;
+  }
+
+  get customer() {
+    return _customer;
+  }
+}
+
+class Customer {
+  constructor(name, billingPlan, paymentHistory) {
+    this._name = name;
+    this._billingPlan = billingPlan;
+    this._paymentHistory = paymentHistory;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get billingPlan() {
+    return this._billingPlan;
+  }
+
+  set billingPlan(arg) {
+    this._billingPlan = arg;
+  }
+
+  get paymentHistory() {
+    return this._paymentHistory;
+  }
+}
+
+const client1 = () => {
+  const customer = new Site().customer;
+  //...
+  let customerName;
+  if (customer === "unknown") customerName = "occupant";
+  else customerName = customer.name;
+};
+const client2 = () => {
+  const customer = new Site().customer;
+  const plan = customer === "unknown" ? registry.billingPlans.basic : customer.billingPlan;
+};
+const client3 = () => {
+  const customer = new Site().customer;
+  const weeksDelinquent = customer === "unknown" ? 0 : customer.paymentHsitry.weeksDelinquentInLastYear;
+};
