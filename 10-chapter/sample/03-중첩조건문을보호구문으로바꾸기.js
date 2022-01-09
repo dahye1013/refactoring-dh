8e9f66e45f36ad0a72357ada20f691e6bb03aede
dahye1013 <alrean@naver.com>
2022-01-09 11:05:13 +0900

    [ch10] 10-03 중첩조건문 - 보호구문

--- 10-chapter/sample/03-중첩조건문을보호구문으로바꾸기.js
+++ 10-chapter/sample/03-중첩조건문을보호구문으로바꾸기.js
@@ -1,14 +1,11 @@
+//early return 패턴을 활용하여 보호구문으로 바꾸기
+//ref : https://medium.com/swlh/return-early-pattern-3d18a41bba8
+
 const payAmount = (employee) => {
-  let result;
   //퇴사O
-  if (employee.isSeperated) result = { amount: 0, reasonCode: "SEP" };
-  else {
-    //은퇴O
-    if (employee.isRetired) result = { amount: 0, reasonCode: "RET" };
-    else {
-      // 재직O - 급여 계산 로직
-      result = { amount: 100, reasonCode: "WRK" };
-    }
-  }
-  return result;
+  if (employee.isSeperated) return { amount: 0, reasonCode: "SEP" };
+  //은퇴O
+  if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+  // 재직O - 급여 계산 로직
+  return { amount: 100, reasonCode: "WRK" };
 };
