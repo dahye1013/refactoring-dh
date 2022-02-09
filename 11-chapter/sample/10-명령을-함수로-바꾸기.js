9e9063b4eb08976eeca7ccfc8a3426eecd8eb33b
dahye1013 <alrean@naver.com>
2022-02-09 16:11:30 +0900

    [ch11] 11-10 명령형을 함수로 바꾸기 - 죽은 코드 제거하기

--- 11-chapter/sample/10-명령을-함수로-바꾸기.js
+++ 11-chapter/sample/10-명령을-함수로-바꾸기.js
@@ -1,18 +1,3 @@
-class ChargeCalculator {
-  consturoctor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-  //[함수선언바꾸기]
-  //- 생성자 전달 모든 데이터 주 메소드로 이동
-  //- 필드 대신 전달받은 매개변수 사용
-  charge(customer, usage, provider) {
-    const baseCharge = customer * usage; //보조함수 인라인
-    return baseCharge + provider;
-  }
-}
-
 const charge = (customer, usage, provider) => {
   const baseCharge = customer * usage; //생성자와 메서드 호출 인라인
   return baseCharge + provider;
