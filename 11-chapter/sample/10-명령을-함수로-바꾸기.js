3966abdc3f5ea768d01be54c23cb99c7765e055d
dahye1013 <alrean@naver.com>
2022-02-09 16:11:00 +0900

    [ch11] 11-10 명령형을 함수로 바꾸기 - 함수 인라인하기

--- 11-chapter/sample/10-명령을-함수로-바꾸기.js
+++ 11-chapter/sample/10-명령을-함수로-바꾸기.js
@@ -13,12 +13,10 @@ class ChargeCalculator {
   }
 }
 
-const charge = (customer, usage, provider) =>
-  new ChargeCalculator(customer, usage, provider).charge(
-    customer,
-    usage,
-    provider
-  );
+const charge = (customer, usage, provider) => {
+  const baseCharge = customer * usage; //생성자와 메서드 호출 인라인
+  return baseCharge + provider;
+};
 
 //호출자
 const monthCharge = charge(customer, usage, provider).charge;
