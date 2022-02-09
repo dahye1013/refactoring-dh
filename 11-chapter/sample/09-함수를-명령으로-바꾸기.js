ddb99eb586adf3b2512307a5758740b3eac47a79
dahye1013 <alrean@naver.com>
2022-02-09 15:32:24 +0900

    [ch11] 11-9 함수 명령으로 바꾸기- 명령인수 생성자로 옮기기

--- 11-chapter/sample/09-함수를-명령으로-바꾸기.js
+++ 11-chapter/sample/09-함수를-명령으로-바꾸기.js
@@ -1,16 +1,28 @@
 class Score {
-  excute(candidate, medicalExam, scoringGuide) {
+  /**
+   * [명령 받는 인수를 생성자로 옮기기]
+   * -> 명령의 수명주기나 사용자 정의 기능 지원하게되면 매개변수가 복잡해질 때 편리하다.
+   * -> 하나의 실행 대기열 queue 통해서 전달 사용도 가능해짐
+   */
+  constructor(candidate, medicalExam, scoringGuide) {
+    this._candidate = candidate;
+    this._medicalExam = medicalExam;
+    this._scoringGuide = scoringGuide;
+  }
+  excute() {
     let result = 0;
     let healthLevel = 0;
     let highMedicalRiskFlag = false;
 
-    if (medicalExam.isSmoker) {
+    if (this._medicalExam.isSmoker) {
       healthLevel += 10;
       highMedicalRiskFlag = true;
     }
 
     let certificationGrade = "regular";
-    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+    if (
+      this._scoringGuide.stateWithLowCertification(this._candidate.originState)
+    ) {
       certificationGrade = "low";
       result -= 5;
     }
@@ -21,5 +33,5 @@ class Score {
 }
 
 const score = (candidate, medicalExam, scoringGuide) => {
-  return new Score().excute(candidate, medicalExam, scoringGuide);
+  return new Score(candidate, medicalExam, scoringGuide).excute();
 };
