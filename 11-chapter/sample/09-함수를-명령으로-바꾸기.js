58921d0d4d5c9654f9a80a28d38a4de04fea43c0
dahye1013 <alrean@naver.com>
2022-02-09 15:39:36 +0900

    [ch11] 11-9 함수 명령으로 바꾸기- 지역변수 필드 바꾸기

--- 11-chapter/sample/09-함수를-명령으로-바꾸기.js
+++ 11-chapter/sample/09-함수를-명령으로-바꾸기.js
@@ -1,8 +1,9 @@
 class Score {
   /**
-   * [명령 받는 인수를 생성자로 옮기기]
-   * -> 명령의 수명주기나 사용자 정의 기능 지원하게되면 매개변수가 복잡해질 때 편리하다.
-   * -> 하나의 실행 대기열 queue 통해서 전달 사용도 가능해짐
+   * [지역 변수 필드로 바꾸기]
+   * - 복잡한 함수 잘게 나누기 위한 목적
+   * - 함수 상태를 모두 명령 객체로 옮긴다
+   *   -> 함수가 사용하던 변수를 유효범위에 구애받지 않고 함수 추출하기 리팩토링 적용할 수 있다.
    */
   constructor(candidate, medicalExam, scoringGuide) {
     this._candidate = candidate;
@@ -10,13 +11,13 @@ class Score {
     this._scoringGuide = scoringGuide;
   }
   excute() {
-    let result = 0;
-    let healthLevel = 0;
-    let highMedicalRiskFlag = false;
+    this._result = 0;
+    this._healthLevel = 0;
+    this._highMedicalRiskFlag = false;
 
     if (this._medicalExam.isSmoker) {
-      healthLevel += 10;
-      highMedicalRiskFlag = true;
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
     }
 
     let certificationGrade = "regular";
@@ -24,11 +25,11 @@ class Score {
       this._scoringGuide.stateWithLowCertification(this._candidate.originState)
     ) {
       certificationGrade = "low";
-      result -= 5;
+      this._result -= 5;
     }
 
-    result -= Math.max(healthLevel - 5, 0);
-    return result;
+    this._result -= Math.max(this._healthLevel - 5, 0);
+    return this._result;
   }
 }
 
