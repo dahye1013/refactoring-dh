c24676436c6c131fa07ded8c1b4fac6fcc4caa95
dahye1013 <alrean@naver.com>
2022-02-09 16:07:30 +0900

    [ch11] 11-10 명령형을 함수로 바꾸기 - 생성자 매개변수 명령 실행 메서드로 이동

--- 11-chapter/sample/10-명령을-함수로-바꾸기.js
+++ 11-chapter/sample/10-명령을-함수로-바꾸기.js
@@ -4,14 +4,21 @@ class ChargeCalculator {
     this._usage = usage;
     this._provider = provider;
   }
-  get charge() {
-    const baseCharge = this._customer.baseRate * this._usage; //보조함수 인라인
-    return baseCharge + this._provider.connectionCharge;
+  //[함수선언바꾸기]
+  //- 생성자 전달 모든 데이터 주 메소드로 이동
+  //- 필드 대신 전달받은 매개변수 사용
+  charge(customer, usage, provider) {
+    const baseCharge = customer * usage; //보조함수 인라인
+    return baseCharge + provider;
   }
 }
 
 const charge = (customer, usage, provider) =>
-  new ChargeCalculator(customer, usage, provider).charge;
+  new ChargeCalculator(customer, usage, provider).charge(
+    customer,
+    usage,
+    provider
+  );
 
 //호출자
 const monthCharge = charge(customer, usage, provider).charge;
